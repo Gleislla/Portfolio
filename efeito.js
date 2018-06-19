58e915bf74b3d62ce1b8db90a577474bb5a64f8d
Gleislla <38674295+Gleislla@users.noreply.github.com>
2018-06-18 23:58:35 -0300

    Trabalhos
    Portfólio para a cadeira de Autorção Multimídia 2

---
+++ efeito.js
@@ -0,0 +1,14 @@
+$( document ).ready(function() {
+	$("body").hide();
+		$("body").fadeIn(1300);
+	$("#imagens img").hide();
+		$("#imagens img").slideDown(1400);
+	$("#enviar").on("click", function() {
+	  alert("Esta função não está funcionando no momento. Por favor, cheque minhas redes sociais.");
+	});
+	
+});
+
+
+
+
